bd42fe819a122939883f4d78a700a3b3ed3ed823
csssuf <csssuf724@gmail.com>
2013-10-24 10:04:19 -0400

    add simple event tree

--- webserver.py
+++ webserver.py
@@ -19,16 +19,43 @@ class IndexHandler(web.RequestHandler):
         self.render("index.html", config=cfgarr)
 
 class SocketHandler(websocket.WebSocketHandler):
+    # State indicates the client's progress in opening a connection.
+    # 0 = no data received, not even VNC server data
+    # 1 = vnc server data received, opening connection
+    # 2 = vnc connection established
     def open(self):
         self.set_nodelay(True)
+        self.state = 0
 
     def on_close(self):
         print('lost client')
 
     def on_message(self, message):
         self.write_message('RECEIVED: ' + message)
+        splitmsg = message.split(' ')
+        if self.state == 0 and message.split(' ')[0][:6] == 'config':
+            check_config(message)
+        elif self.state == 2:
+            if splitmsg[0] == 'mouse_move':
+                do_move(msg[1:])
+            elif splitmsg[0] == 'mouse_down' or splitmsg[0] == 'mouse_up':
+                do_mouse((splitmsg[0] == 'mouse_down'), msg[1:])
+            elif splitmsg[0] == 'key_down' or splitmsg[0] == 'key_up':
+                do_key((splitmsg[0] == 'key_down'), msg[1])
 
     def select_subprotocol(self, subprotocols):
+        pass
+
+    def check_config(msg):
+        pass
+
+    def do_move(msgarr):
+        pass
+
+    def do_mouse(mstate, msgarr):
+        pass
+
+    def do_key(keystate, msgarr):
         pass
 
 if __name__ == '__main__':
